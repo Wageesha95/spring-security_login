e8a20b471f05fe91de2252eaac1c3ee602575d2e
Thilina <imwageesgha@gmail.com>
2020-05-15 13:46:27 +0530

    test with default password encorder - 1

---
+++ src/main/java/com/example/sbsecuritytrial/SbSecurityTrial01Application.java
@@ -0,0 +1,13 @@
+package com.example.sbsecuritytrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityTrial01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityTrial01Application.class, args);
+	}
+
+}
